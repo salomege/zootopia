6c84c1458126b93682cf72ba405b8534abca4a8a
salomege <salomegelenidze@gmail.com>
2022-04-04 11:24:46 +0400

    add slider

--- app.js
+++ app.js
@@ -14,3 +14,23 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+
+{
+	const productContainers = [...document.querySelectorAll('.product-container-div')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+}
